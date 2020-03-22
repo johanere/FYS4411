5aae711b1d6a5a40e4e1ea2651fc177f4aa082b7
johanere <42669090+johanere@users.noreply.github.com>
2020-03-22 21:39:21 +0100

    Initial commit

---
+++ Code/WaveFunctions/simplegaussian.cpp
@@ -0,0 +1,52 @@
+#include "simplegaussian.h"
+#include <cmath>
+#include <cassert>
+#include "wavefunction.h"
+#include "../system.h"
+#include "../particle.h"
+
+//remove
+#include <iostream>
+using namespace std;
+//
+
+SimpleGaussian::SimpleGaussian(System* system, double alpha) :
+        WaveFunction(system) {
+    assert(alpha >= 0);
+    m_numberOfParameters = 1;
+    m_parameters.reserve(1);
+    m_parameters.push_back(alpha);
+}
+
+double SimpleGaussian::evaluate(std::vector<class Particle*> particles) {
+    /* You need to implement a Gaussian wave function here. The positions of
+     * the particles are accessible through the particle[i].getPosition()
+     * function.
+     *
+     * For the actual expression, use exp(-alpha * r^2), with alpha being the
+     * (only) variational parameter.
+     */
+    double r_squared,g=1;
+    for (int i=0; i<m_system->getNumberOfParticles();i++)
+    {
+      r_squared=0;
+      for (int dimension=0; dimension <particles[i]->getPosition().size();dimension++)
+      {
+        r_squared+=pow(particles[i]->getPosition()[dimension],2);
+      }
+    g*=exp(-m_parameters[0]*r_squared);  //exp ( alpha * r^2)
+    }
+    return g;
+}
+
+double SimpleGaussian::computeDoubleDerivative(std::vector<class Particle*> particles) {
+    /* All wave functions need to implement this function, so you need to
+     * find the double derivative analytically. Note that by double derivative,
+     * we actually mean the sum of the Laplacians with respect to the
+     * coordinates of each particle.
+     *
+     * This quantity is needed to compute the (local) energy (consider the
+     * Schrödinger equation to see how the two are related).
+     */
+    return 0;
+}
